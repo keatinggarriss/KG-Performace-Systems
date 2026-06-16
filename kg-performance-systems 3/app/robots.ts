3463c50e1cf6f434bca9956049a42bca3518877e
keatinggarriss <keatinggarrissbusiness@gmail.com>
2026-06-15 20:53:29 -0700

    Delete kg-performance-systems 3 directory

--- kg-performance-systems 3/app/robots.ts
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://www.kgperformancesystems.com/sitemap.xml", // REPLACE domain
-  };
-}
